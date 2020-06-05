7cf58dcc19c57bb872da309e4107f25aeeb39446
Jhordy Isensee <jhordy@jhordy.lan>
2020-06-05 12:34:26 +0100

    finished

---
+++ src/components/ModalAddFood/index.tsx
@@ -0,0 +1,93 @@
+import React, { useRef, useCallback } from 'react';
+import * as Yup from 'yup';
+
+import { FiCheckSquare } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+interface IFoodPlate {
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
+
+interface ICreateFoodData {
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+}
+
+interface IModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleAddFood: (food: Omit<IFoodPlate, 'id' | 'available'>) => void;
+}
+
+const ModalAddFood: React.FC<IModalProps> = ({
+  isOpen,
+  setIsOpen,
+  handleAddFood,
+}) => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(
+    async (data: ICreateFoodData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Please, write a food name'),
+          image: Yup.string().required('Please, add a food image'),
+          price: Yup.number().required('Please, add a food value'),
+          description: Yup.string().required(
+            'Please, write a food description',
+          ),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await handleAddFood(data);
+
+        setIsOpen();
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+        }
+      }
+    },
+    [handleAddFood, setIsOpen],
+  );
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
+
+export default ModalAddFood;
